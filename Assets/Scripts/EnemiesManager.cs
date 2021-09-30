5cedbe5a860c13e2c11f6c0db8884688ff7102b5
Anna Derkachenko <anyaderkachenko@gmail.com>
2021-09-30 21:32:09 +0300

    Try to merge branches

---
+++ Assets/Scripts/EnemiesManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesManager : MonoBehaviour
+{
+    public List<GameObject> enemies;
+    [Range(0f, 50f)]
+    public float SpawnTime, EnemiesCount;
+    private float _currentCount, _groundWidth;
+
+    void Start()
+    {
+        _currentCount = 0;
+        _groundWidth = 9f;
+    }
+
+    private IEnumerator InitEnemies(int number)
+    {
+        while (_currentCount < EnemiesCount)
+        {
+            float time = Random.Range(0f, SpawnTime);
+            Vector3 position = new Vector3(Random.Range(-_groundWidth, _groundWidth), 0, Random.Range(-_groundWidth, _groundWidth));
+            GameObject enemy = Instantiate(enemies[number], position, Quaternion.Euler(0f, Random.Range(-180f, 180f), 0f));
+            ++_currentCount;
+            yield return new WaitForSeconds(time);
+        }
+    }
+
+    void Update()
+    {
+        StartCoroutine(InitEnemies(0));
+    }
+}
--- Assets/Scripts/MachineGun.cs
+++ Assets/Scripts/MachineGun.cs
@@ -8,7 +8,6 @@ public class MachineGun : BaseGun
     {
         GameObject bullet = Instantiate(BulletPrefab, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z),
             transform.rotation * Quaternion.Euler(90f, 0f, 0f));
-        //при движении все ломается
-        bullet.GetComponent<BaseBullet>().Direction = gameObject.GetComponent<PlayerMovement>().Direction;
+        bullet.GetComponent<BaseBullet>().Direction = gameObject.GetComponent<PlayerMovement>().direction;
     }
 }
