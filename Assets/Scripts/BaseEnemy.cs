4e2de08e7b504aa2b84615ed795ae241a87a106e
Anna Derkachenko <anyaderkachenko@gmail.com>
2021-10-30 00:38:38 +0300

    Normalize distance between the player and a enemy

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -17,7 +17,7 @@ public class BaseEnemy : MonoBehaviour
     private Rigidbody _rigidBody;
     private Vector3 _targetPosition;
     private Animator _animator;
-    private bool _isPlayerExist;
+    private bool _isPlayerExists;
     private bool _isAttacking;
     private float _hp;
     private float _attackSpeed;
@@ -31,16 +31,22 @@ public class BaseEnemy : MonoBehaviour
         _hp = Health.Value;
     }
 
-    public void MoveTo(Vector3 position) => _targetPosition = (position - transform.position) * Rapidity.Value;
+    public void MoveTo(Vector3 position)
+    {
+        _targetPosition = position - transform.position;
+        if (_targetPosition.magnitude >= 1)
+            _targetPosition = _targetPosition.normalized;
+    }
 
     public void Stay()
     {
-        _isPlayerExist = false;
+        _isPlayerExists = false;
         _animator.SetBool("isMoving", false);
     }
 
     public void TakeDamage(float power)
     {
+        // TODO: when an enemy dies, sometimes it deals damage or the death animation doesn't play
         if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Death")) return;
         _hp -= power;
         HealthBar.fillAmount = _hp / Health.Value;
@@ -56,7 +62,7 @@ public class BaseEnemy : MonoBehaviour
         Power.Init();
         DeathPoints.Init();
 
-        _isPlayerExist = true;
+        _isPlayerExists = true;
         _rigidBody = gameObject.GetComponent<Rigidbody>();
         _animator = gameObject.GetComponent<Animator>();
         _hp = Health.Value;
@@ -73,11 +79,12 @@ public class BaseEnemy : MonoBehaviour
 
     private IEnumerator Attack(GameObject target)
     {
-        while (_isAttacking)
+        while (_isPlayerExists && _isAttacking)
         {
-            if (target == null || _animator.GetCurrentAnimatorStateInfo(0).IsName("Death")) yield break;
             _animator.SetTrigger("Attack");
-            target.GetComponent<Player>().TakeDamage(Power.Value);
+            yield return new WaitForSeconds(0.8f);
+            if (target != null)
+                target.GetComponent<Player>().TakeDamage(Power.Value);
             yield return new WaitForSeconds(_attackSpeed);
         }
     }
@@ -88,26 +95,25 @@ public class BaseEnemy : MonoBehaviour
         if (target.CompareTag("Player") && !_isAttacking)
         {
             _isAttacking = true;
+            _animator.SetBool("isMoving", false);
             StartCoroutine(Attack(target));
         }
     }
 
-    private void OnTriggerExit(Collider collider) => _isAttacking = false;
-
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerExit(Collider collider)
     {
-        /*if (collision.gameObject.tag == "Enemy")
-        //need to fix collision between enemies
-        */
+        _isAttacking = false;
+        _animator.SetBool("isMoving", true);
     }
 
     private void FixedUpdate()
     {
-        if (_isPlayerExist)
+        if (_isPlayerExists)
         {
-            Vector3 targetPos = _targetPosition * Time.fixedDeltaTime;
+            Vector3 targetPos = _targetPosition * Rapidity.Value * Time.fixedDeltaTime;
             transform.rotation = Quaternion.LookRotation(targetPos);
-            _rigidBody.MovePosition(transform.position + targetPos);
+            if (!_isAttacking)
+                _rigidBody.MovePosition(transform.position + targetPos);
         }
     }
 }
