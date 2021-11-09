d189a2e565de696371460f58f358eb5d5f5b06a7
Anna Derkachenko <60828039+derkanw@users.noreply.github.com>
2021-11-09 13:50:49 +0300

    Update Assets/Scripts/Tokens.cs
    Co-authored-by: Anton Pushkin <32176025+pushkinman@users.noreply.github.com>

--- Assets/Scripts/Tokens.cs
+++ Assets/Scripts/Tokens.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public static class Tokens
 {
-    public static readonly string StatsPoints = "PointsForStats";
-    public static readonly string Stats = "PlayerStats";
-    public static readonly string PlayerLevel = "PlayerLevel";
-    public static readonly string Points = "PointsForNewLevel";
-    public static readonly string GameLevel = "GameLevel";
-    public static readonly string EnemyIndex = "EnemyIndex";
+    public static const string StatsPoints = "PointsForStats";
+    public static const string Stats = "PlayerStats";
+    public static const string PlayerLevel = "PlayerLevel";
+    public static const string Points = "PointsForNewLevel";
+    public static const string GameLevel = "GameLevel";
+    public static const string EnemyIndex = "EnemyIndex";
 }
